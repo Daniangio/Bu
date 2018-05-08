91e52decf9e82abb18e5ff899182e26fdf30f9fb
Daniangio <daniele.angioletti0@gmail.com>
2018-05-08 12:10:58 +0200

    Aggiunto menù

---
+++ Assets/Scripts/OpenPanelOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanelOnClick : MonoBehaviour {
+
+	bool panelOpen = false;
+	public GameObject defaultPanel;
+	GameObject currentPanel;
+
+	public void OpenPanel() {
+		currentPanel = GameObject.FindGameObjectWithTag ("OptionsPanel");
+		if (currentPanel == null)
+			currentPanel = defaultPanel;
+		
+		panelOpen = !panelOpen;
+		currentPanel.SetActive(panelOpen);
+	}
+
+	public void SwitchPanel() {
+		defaultPanel.SetActive(true);
+		this.transform.parent.gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsManager : MonoBehaviour {
+
+	public int levelDifficulty = 1;
+
+	public void SetGameDifficulty(int level) {
+		levelDifficulty = level;
+	}
+
+	void OnEnable() {
+		GameObject levelButton = null;
+		switch(levelDifficulty) {
+		case 1:
+			levelButton = GameObject.Find ("EasyLevelButton");
+			break;
+		case 2:
+			levelButton = GameObject.Find ("HardLevelButton");
+			break;
+		default:
+			break;
+		}
+		
+		GameObject eventSystem = GameObject.Find ("EventSystem");
+		eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem> ().SetSelectedGameObject (levelButton);
+	}
+}
